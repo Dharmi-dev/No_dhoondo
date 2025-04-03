7c535b11c7c0f52dadfc7343f2463f781936fa81
Dharmesh Tanwar <dharmesh230335@keshav.du.ac.in>
2025-04-03 12:30:58 +0530

    initiallize the project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Is everyone ready? to work </h1>
+        <h2>How is the josh?</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
